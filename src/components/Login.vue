bcf2536d660585e9c10228503e5693e1bc9366f3
Marcus Pratley <mpratley@and.digital>
2019-04-02 23:42:35 +0100

    Revert "Complete rework of google login"
    This reverts commit 8969e17a7e219790d2227fbda4e651f9331378f1.
    
    Due to an error on my part

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,17 @@
 <template>
-  <div id="firebaseui-auth-container"></div>
+  <div id="login-view">
+    <button @click="signIn">Sign In!</button>
+  </div>
 </template>
 
 <script>
-import firebase from "firebase/app";
-import "firebase/auth";
-import config from "@/firebaseConfig.js";
-import firebaseui from "firebaseui";
-
-const uiConfig = {
-  signInSuccessUrl: "/",
-  signInOptions: [
-    {
-      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      scopes: config.scopes
-    }
-  ]
-};
-
 export default {
-  created() {
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start("#firebaseui-auth-container", uiConfig);
+  methods: {
+    signIn() {
+      this.$getGapiClient().then(gapi => {
+        gapi.auth2.getAuthInstance().signIn();
+      });
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,29 +3,36 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
 import "./registerServiceWorker";
-//Gapi and Firebase
 import VueGAPI from "vue-gapi";
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "@/firebaseConfig.js";
 
-const gapiConfig = {
-  apiKey: firebaseConfig.apiKey,
-  clientId: firebaseConfig.clientID,
-  discoveryDocs: firebaseConfig.discoveryDocs,
-  scope: firebaseConfig.scopes.join(" ")
+const config = {
+  apiKey: "AIzaSyC8z7jhuADsz_FsikCx2xfN8OlQBvzGiBE",
+  clientId:
+    "205275658111-ho20gfi8uenlar4s89letdpnt15jilm8.apps.googleusercontent.com",
+  scope: [
+    "email",
+    "profile",
+    "https://www.googleapis.com/auth/admin.directory.user",
+    "https://www.googleapis.com/auth/admin.directory.user.readonly"
+  ].join(" "),
+  discoveryDocs: [
+    "https://www.googleapis.com/discovery/v1/apis/admin/directory_v1/rest"
+  ]
 };
 
 Vue.config.productionTip = false;
-Vue.use(VueGAPI, gapiConfig);
+Vue.use(VueGAPI, config);
 
 new Vue({
   router,
   store,
   created() {
-    firebase.initializeApp(firebaseConfig);
-    firebase.auth().onAuthStateChanged(user => {
-      this.$store.commit("updateAuthStatus", user != null);
+    this.$getGapiClient().then(gapi => {
+      this.$store.commit("updateAuthStatus", this.$isAuthenticated);
+      gapi.auth2.getAuthInstance().isSignedIn.listen(boolean => {
+        this.$store.commit("updateAuthStatus", boolean);
+        console.log("sign in status: " + boolean);
+      });
     });
   },
   render: h => h(App)
