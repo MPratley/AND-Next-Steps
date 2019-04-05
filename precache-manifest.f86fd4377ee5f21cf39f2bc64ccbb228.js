2c3a9eb5d0ee39de22442756ff780d510c608a40
Marcus Pratley <mpratley@and.digital>
2019-04-05 10:34:36 +0100

    Updates

---
+++ precache-manifest.f86fd4377ee5f21cf39f2bc64ccbb228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/AND-Next-Steps/robots.txt"
+  },
+  {
+    "revision": "2c2fc97a224242796f4e",
+    "url": "/AND-Next-Steps/js/chunk-vendors.09cc6ce8.js"
+  },
+  {
+    "revision": "766677427ca9847e598f",
+    "url": "/AND-Next-Steps/js/app.0f940a81.js"
+  },
+  {
+    "revision": "7c4bbb946e2eb9895e96d51675611647",
+    "url": "/AND-Next-Steps/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/AND-Next-Steps/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "766677427ca9847e598f",
+    "url": "/AND-Next-Steps/css/app.e593072b.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AND-Next-Steps/precache-manifest.3481bd12eb2e2b3614b5dce0042e2a3d.js"
+  "/AND-Next-Steps/precache-manifest.f86fd4377ee5f21cf39f2bc64ccbb228.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "and-next-steps"});
