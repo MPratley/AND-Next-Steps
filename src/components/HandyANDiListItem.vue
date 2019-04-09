0cf6b12b11ad935063bd8a8b1897962a6c815df2
Marcus Pratley <mpratley@and.digital>
2019-04-09 17:56:31 +0100

    material card test for handyandi

--- src/components/HandyANDiListItem.vue
+++ src/components/HandyANDiListItem.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="listItem">
-    <p>{{ andi.relation }}</p>
+  <div class="mdc-card">
+    <div class="mdc-card__media mdc-card__media--16-9">
+      <h2 class="mdc-card__media-content">{{ gapiAndi.name.fullName }}</h2>
+    </div>
+  </div>
+  <!-- <p>{{ andi.relation }}</p>
     <p v-if="!gapiAndi">{{ andi.email }}</p>
     <div v-else>
       <p>{{ gapiAndi.name.fullName }}</p>
       <div class="ANDi">
-        <img class="thumbnail" :src="gapiAndi.thumbnailPhotoUrl" />
+        <img class="thumbnail" :src="gapiAndi.thumbnailPhotoUrl">
       </div>
-    </div>
-  </div>
+  </div>-->
 </template>
 
 <script>
@@ -38,23 +41,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#listItem {
-  .thumbnail {
-    height: 100%;
-  }
-  text-align: left;
-  background: #fff;
-  border-radius: 2px;
-  width: 80%;
-  height: 110px;
-  display: block;
-  margin: 1rem;
-  position: relative;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.1s cubic-bezier(0.25, 0.8, 0.25, 1);
-  &:hover {
-    box-shadow: 0 14px 28px rgba(124, 124, 124, 0.25),
-      0 10px 10px rgba(124, 124, 124, 0.22);
-  }
-}
+@import "~@material/card/mdc-card";
+
+// #listItem {
+//   .thumbnail {
+//     height: 100%;
+//   }
+//   text-align: left;
+//   background: #fff;
+//   border-radius: 2px;
+//   width: 80%;
+//   height: 110px;
+//   display: block;
+//   margin: 1rem;
+//   position: relative;
+//   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+//   transition: all 0.1s cubic-bezier(0.25, 0.8, 0.25, 1);
+//   &:hover {
+//     box-shadow: 0 14px 28px rgba(124, 124, 124, 0.25),
+//       0 10px 10px rgba(124, 124, 124, 0.22);
+//   }
+// }
 </style>
