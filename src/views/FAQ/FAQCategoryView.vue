ff86eab310370eb6ae86d851e0508ae527540cc0
Kay Bennett <kaybennett@MacBook-Pro-4.local>
2019-04-15 10:16:19 +0100

    category page and back button working

--- src/views/FAQ/FAQCategoryView.vue
+++ src/views/FAQ/FAQCategoryView.vue
@@ -1,22 +1,100 @@
 <template>
-  <div class="category-box">
+  <div class="category">
     <h2 class="title">Categories</h2>
+    <div class="row">
+      <div class="column">
+        <div class="category-column" @click="() => selectCategory('Club')">
+          <i class="material-icons category-icon">home</i>
+          <br />Club
+        </div>
+      </div>
+      <div class="column">
+        <div class="category-column" @click="() => selectCategory('Company')">
+          <i class="material-icons category-icon">work</i>
+          <br />Company
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column">
+        <div class="category-column" @click="() => selectCategory('Me')">
+          <i class="material-icons category-icon">face</i>
+          <br />Me
+        </div>
+      </div>
+      <div class="column">
+        <div class="category-column" @click="() => selectCategory('Client')">
+          <i class="material-icons category-icon">domain</i>
+          <br />Client
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  name: "FAQCategoryView"
+  name: "FAQCategoryView",
+  data() {
+    return {
+      category: ""
+    };
+  },
+
+  methods: {
+    selectCategory: function(category) {
+      console.log(category);
+      console.log(arguments);
+      this.category = category;
+      this.$store.dispatch("changeSearch", category);
+      this.$store.dispatch("searchCategory");
+      this.$router.push("/faq/search");
+    }
+  },
+
+  computed: {
+    ...mapActions(["changeSearch"])
+  }
 };
 </script>
 
 <style lang="scss">
-.category-box {
-  height: 400px;
-  width: 400px;
+.category-icon {
+  font-size: 60px;
+  opacity: 0.7;
 }
-.title {
-  color: $and-blue;
+
+.category {
+  margin: 15px;
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  line-height: normal;
   text-align: center;
+  color: #323232;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.column {
+  display: flex;
+  padding: 1em;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+}
+
+.category-column {
+  background-color: #fafafa;
+  height: 7.5em;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.24), 0px 0px 2px rgba(0, 0, 0, 0.12);
+  border-radius: 2px;
 }
 </style>
