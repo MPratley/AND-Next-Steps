c91edb1ab5dc607b2dbf3eb7ca183a9eedbffdde
Marcus Pratley <mpratley@and.digital>
2019-04-17 16:26:47 +0100

    button goes wibble

---
+++ src/views/HandiANDi/HandyANDiAddNew.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="createNewAndi">
+    <ui-autocomplete
+      name="email"
+      help="Type the email of this ANDi"
+      label="Email"
+      placeholder="Email"
+      :suggestions="employees"
+      v-model="email"
+      @input="update"
+      :keys="{
+        label: 'primaryEmail',
+        value: 'primaryEmail',
+        image: 'primaryEmail'
+      }"
+      :filter="filter"
+    ></ui-autocomplete>
+    <label for="role">Role of ANDi</label>
+    <select id="role" v-model="role" name="role">
+      <option>Squad Lead</option>
+      <option>Dev Coach</option>
+      <option>PDL</option>
+      <option>Buddy</option>
+    </select>
+    <button type="submit" @click="add">create</button>
+  </div>
+</template>
+
+<script>
+import { UiAutocomplete } from "keen-ui";
+
+export default {
+  name: "CreateNewAndi",
+  components: {
+    UiAutocomplete
+  },
+  data() {
+    return {
+      role: "",
+      email: "",
+      employees: []
+    };
+  },
+  methods: {
+    add() {
+      this.$store.dispatch("user/set", {
+        handyAndi: [
+          ...this.$store.state.user.data.handyAndi,
+          { email: this.email, relation: this.role }
+        ]
+      });
+      this.$router.go(-1);
+    },
+    update() {
+      if (this.email.length > 3) {
+        this.$getGapiClient().then(gapi => {
+          gapi.client.directory.users
+            .list({
+              customer: "my_customer",
+              viewType: "domain_public",
+              query: this.email
+            })
+            .then(data => {
+              this.employees = data.result.users;
+            });
+        });
+      }
+    },
+    filter(suggestion, query) {
+      if (
+        suggestion.primaryEmail.toLowerCase().includes(query.toLowerCase()) ||
+        suggestion.name.fullName.toLowerCase().includes(query.toLowerCase())
+      ) {
+        return true;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HandyANDi.vue
+++ src/views/HandyANDi.vue
@@ -1,53 +1,29 @@
 <template>
   <div class="handy-andi-list">
     <h1>Your ANDi</h1>
-    <div v-if="createNewAndi" class="createNewAndi">
-      <ui-autocomplete
-        name="email"
-        help="Type the email of this ANDi"
-        label="Email"
-        placeholder="Email"
-        :suggestions="employees"
-        v-model="email"
-        @input="update"
-        :keys="{
-          label: 'primaryEmail',
-          value: 'primaryEmail',
-          image: 'primaryEmail'
-        }"
-        :filter="filter"
-      ></ui-autocomplete>
-      <label for="role">Role of ANDi</label>
-      <select id="role" v-model="role" name="role">
-        <option>Squad Lead</option>
-        <option>Dev Coach</option>
-        <option>PDL</option>
-        <option>Buddy</option>
-      </select>
-      <button type="submit" @click="add">create</button>
-    </div>
     <div v-for="andi in handyAndi" :key="andi.email">
       <HandyANDiListItem :andi="andi" class="andi" />
     </div>
     <button
-      class="mdc-fab mdc-fab--extended new-andi"
-      @click="toggleCreateNewAndi"
+      class="mdc-fab new-andi-fab"
+      @click="createNewAndi"
+      :class="{ 'mdc-fab--extended': bigFab, 'new-andi-fab--big': bigFab }"
     >
       <span class="material-icons mdc-fab__icon">add</span>
-      <span class="mdc-fab__label">New Andi</span>
+      <span v-if="bigFab" class="mdc-fab__label">New Andi</span>
     </button>
   </div>
 </template>
 
 <script>
 import HandyANDiListItem from "@/components/HandyANDiListItem";
-import { UiAutocomplete } from "keen-ui";
+// import { UiAutocomplete } from "keen-ui";
 
 export default {
-  name: "Handy Andi",
+  name: "HandyAndi",
   components: {
-    HandyANDiListItem,
-    UiAutocomplete
+    HandyANDiListItem
+    // UiAutocomplete
   },
   computed: {
     handyAndi() {
@@ -56,47 +32,62 @@ export default {
   },
   data() {
     return {
-      createNewAndi: false,
-      role: "",
-      email: "",
-      employees: []
+      bigFab: true
     };
   },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
   methods: {
-    toggleCreateNewAndi() {
-      this.createNewAndi = !this.createNewAndi;
+    createNewAndi() {
+      this.$router.push("/handy-andi/new");
     },
-    add() {
-      this.$store.dispatch("user/set", {
-        handyAndi: [
-          ...this.handyAndi,
-          { email: this.email, relation: this.role }
-        ]
-      });
-    },
-    update() {
-      if (this.email.length > 3) {
-        this.$getGapiClient().then(gapi => {
-          gapi.client.directory.users
-            .list({
-              customer: "my_customer",
-              viewType: "domain_public",
-              query: this.email
-            })
-            .then(data => {
-              this.employees = data.result.users;
-            });
-        });
-      }
-    },
-    filter(suggestion, query) {
-      if (
-        suggestion.primaryEmail.toLowerCase().includes(query.toLowerCase()) ||
-        suggestion.name.fullName.toLowerCase().includes(query.toLowerCase())
-      ) {
-        return true;
+    onScroll() {
+      const currentScrollPosition =
+        window.pageYOffset || document.documentElement.scrollTop;
+      if (currentScrollPosition === 0) {
+        this.bigFab = true;
+      } else {
+        this.bigFab = false;
       }
     }
+    // toggleCreateNewAndi() {
+    //   this.createNewAndi = !this.createNewAndi;
+    // },
+    // add() {
+    //   this.$store.dispatch("user/set", {
+    //     handyAndi: [
+    //       ...this.handyAndi,
+    //       { email: this.email, relation: this.role }
+    //     ]
+    //   });
+    // },
+    // update() {
+    //   if (this.email.length > 3) {
+    //     this.$getGapiClient().then(gapi => {
+    //       gapi.client.directory.users
+    //         .list({
+    //           customer: "my_customer",
+    //           viewType: "domain_public",
+    //           query: this.email
+    //         })
+    //         .then(data => {
+    //           this.employees = data.result.users;
+    //         });
+    //     });
+    //   }
+    // },
+    // filter(suggestion, query) {
+    //   if (
+    //     suggestion.primaryEmail.toLowerCase().includes(query.toLowerCase()) ||
+    //     suggestion.name.fullName.toLowerCase().includes(query.toLowerCase())
+    //   ) {
+    //     return true;
+    //   }
+    // }
   }
 };
 </script>
@@ -116,15 +107,16 @@ h1 {
   margin: 0 auto;
   height: 100px;
 }
-
 .andi {
   margin-bottom: 8px;
 }
-
-.new-andi {
+.new-andi-fab {
   background-color: $color-accent;
   position: fixed;
-  bottom: 100px;
-  transform: translateX(-50%);
+  bottom: 90px;
+  transform: translateX(120px);
+}
+.new-andi-fab--big {
+  transform: translateX(-50%) !important;
 }
 </style>
